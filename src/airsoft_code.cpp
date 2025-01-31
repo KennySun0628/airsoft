c0eb9bef87f56f1cb1099dd2a4cd755dcb179f18
Kenny Sun <kennysun0628@gmail.com>
2025-01-31 13:06:19 -0800

    Init

---
+++ src/airsoft_code.cpp
@@ -0,0 +1,16 @@
+//Sketch1.cpp
+#include <Arduino.h>
+#include "MET.h"
+
+MET m;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(57600);
+  m.run(1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(1000);
+}
